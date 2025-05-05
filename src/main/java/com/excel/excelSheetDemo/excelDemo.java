6a2f4323292757c6fe41f1f92a8605da41a8ac90
Vikashlb <lbvikashbabu10@gmail.com>
2025-05-05 11:18:08 +0530

    Added method to write data in sheet.

--- src/main/java/com/excel/excelSheetDemo/excelDemo.java
+++ src/main/java/com/excel/excelSheetDemo/excelDemo.java
@@ -1,18 +1,19 @@
 package com.excel.excelSheetDemo;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 
 public class excelDemo {
     public static void main(String[] args) {
         excelDemo obj = new excelDemo();
         obj.readData();
+        obj.writeData();
     }
 
     public void readData() {
@@ -50,6 +51,60 @@ public class excelDemo {
             }
         }
         catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void writeData() {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Employees");
+        //sheet.setColumnWidth(0, 4000);
+        //sheet.setColumnWidth(1, 4000);
+        sheet.autoSizeColumn(0);
+        sheet.autoSizeColumn(1);
+
+        Row header = sheet.createRow(0);
+
+        // Create header cell
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_ORANGE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = workbook.createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        Cell headerCell = header.createCell(0);
+        headerCell.setCellValue("Name");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(1);
+        headerCell.setCellValue("Age");
+        headerCell.setCellStyle(headerStyle);
+
+        // Create content cell
+        CellStyle contentStyle = workbook.createCellStyle();
+        contentStyle.setWrapText(true);
+
+        Row row = sheet.createRow(1);
+        Cell contentCell = row.createCell(0);
+        contentCell.setCellValue("Vikash L B");
+        contentCell.setCellStyle(contentStyle);
+
+        contentCell = row.createCell(1);
+        contentCell.setCellValue(22);
+        contentCell.setCellStyle(contentStyle);
+
+        // write data into a file
+        try {
+            File curr = new File("data.xlsx");
+            FileOutputStream outputStream = new FileOutputStream(curr);
+            workbook.write(outputStream);
+            workbook.close();
+            System.out.println("File Created - 'data.xlsx'  ");
+        }
+        catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
